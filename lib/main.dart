864c11d735ef6c1805711198a9b06f028d70b527
Rasheduzzaman Rochi <rasheduzzaman2003@gmail.com>
2024-11-09 23:09:13 +0600

    Container,BoxShadow, BoxDecoration, Container_Border,DecorationImage.

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,6 @@ class MyApp extends StatelessWidget {
 class Practice extends StatelessWidget {
   Practice({super.key});
 
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,73 +30,63 @@ class Practice extends StatelessWidget {
         backgroundColor: Colors.lightBlueAccent,
         toolbarHeight: 63,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Form(
-            key: _formkey,
-            child: Column(
-              children: [
-                TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                padding: EdgeInsets.all(8),
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(15),
+                child: Text("Random"),
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      offset: Offset(2, 2),
+                      blurRadius: 7,
+                      spreadRadius: 5,
                     ),
-                    suffixIcon: Icon(Icons.email),
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return "Enter your email";
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  obscureText: true,
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      offset: Offset(-2, -2),
+                      blurRadius: 5,
+                      spreadRadius: 7,
                     ),
-                    suffixIcon: Icon(Icons.lock),
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty || value.length < 8) {
-                      return "Enter a password with at least 8 characters";
-                    }
-                    return null;
-                  },
+                  ],
                 ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    // String email = _emailController.text;
-                    // String password = _passwordController.text;
-                    // if (email.isNotEmpty && password.isNotEmpty) {
-                    //   print("Login Success");
-                    // } else
-                    //   print("Login Failure");
-                    if (_formkey.currentState!.validate()) {
-                      print("Login Success");
-                    }
-                  },
-                  child: Text(
-                    "Login",
-                    style: TextStyle(color: Colors.white),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                margin: EdgeInsets.only(left: 4),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  border: Border.all(color: Colors.black, width: 3),
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    bottomRight: Radius.circular(16),
                   ),
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent),
                 ),
-              ],
-            ),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                margin: const EdgeInsets.only(left: 10),
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.black),
+                  image: const DecorationImage(
+                      image: AssetImage("images/Shoe.png"), fit: BoxFit.cover),
+                ),
+              ),
+            ],
           ),
-        ),
+        ],
       ),
     );
   }
