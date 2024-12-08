dd6d2ffa3e741cb759319cf497680a1405a273ba
Rasheduzzaman Rochi <rasheduzzaman2003@gmail.com>
2024-12-08 23:56:58 +0600

    Added SafeArea in body with Row -> Expanded, child: Container,flex

--- lib/main.dart
+++ lib/main.dart
@@ -27,29 +27,51 @@ class Home extends StatelessWidget {
       //   aspectRatio: 14/10,
       //   child: ColoredBox(color: Colors.orange),
       // ),
-      body: Column(
-        children: [
-          Flexible(
-              fit: FlexFit.tight,
-              flex: 4,
-              child: SizedBox(
-                width: double.maxFinite,
-                height: 100,
-                child: ColoredBox(color: Colors.black),
-              )),
-          Expanded(
-              flex: 3,
-              child: SizedBox(
-                width: double.maxFinite,
-                height: 100,
-                child: ColoredBox(color: Colors.orange),
-              )),
-          SizedBox(
-            width: double.maxFinite,
-            height: 50,
-            child: ColoredBox(color: Colors.blue),
-          )
-        ],
+      // body: Column(
+      //   children: [
+      //     Flexible(
+      //         fit: FlexFit.tight,
+      //         flex: 4,
+      //         child: SizedBox(
+      //           width: double.maxFinite,
+      //           height: 100,
+      //           child: ColoredBox(color: Colors.black),
+      //         )),
+      //     Expanded(
+      //         flex: 3,
+      //         child: SizedBox(
+      //           width: double.maxFinite,
+      //           height: 100,
+      //           child: ColoredBox(color: Colors.orange),
+      //         )),
+      //     SizedBox(
+      //       width: double.maxFinite,
+      //       height: 50,
+      //       child: ColoredBox(color: Colors.blue),
+      //     )
+      //   ],
+      // ),
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+                child: Container(
+                  color: Colors.orange,
+                  height: 100,
+                )),
+            Expanded(
+                flex: 4,
+                child: Container(
+                  color: Colors.red,
+                  height: 100,
+                )),
+            Expanded(
+                child: Container(
+                  color: Colors.green,
+                  height: 100,
+                )),
+          ],
+        ),
       ),
     );
   }
