2075dae127a7c80328aafec2bf268853279bae08
Rasheduzzaman Rochi <rasheduzzaman2003@gmail.com>
2024-12-19 12:41:40 +0600

    Add todo basic functionality

--- lib/Screens/todo_screen.dart
+++ lib/Screens/todo_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ostad_rafat/Screens/add_new_todo.dart';
+import 'package:ostad_rafat/Screens/todo.dart';
+import 'package:ostad_rafat/Screens/update_todo.dart';
 
 class todoScreen extends StatefulWidget {
   const todoScreen({super.key});
@@ -9,29 +11,120 @@ class todoScreen extends StatefulWidget {
 }
 
 class _todoScreenState extends State<todoScreen> {
+  final List<Todo> listofTodo = [];
+
+  void _addTodo(Todo todo) {
+    listofTodo.add(todo);
+    setState(() {});
+  }
+
+  void _deleteTodo(int index) {
+    listofTodo.removeAt(index);
+    setState(() {});
+  }
+
+  void _updateTodo(int index, Todo todo) {
+    listofTodo[index] = todo;
+    setState(() {});
+  }
+
+  void _updateTodoStatus(int index, TodoStatus status) {
+    listofTodo[index].status = status;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('To-Do List')),
-      body: ListView.builder(
-          itemCount: 3,
-          itemBuilder: (context, index) {
-            return const ListTile(
-              title: Text('To-Do List'),
-              subtitle: Text('description'),
-              trailing: Text('Status'),
-              onTap:(){
-
-              }
-            );
-          }),
+      body: Visibility(
+        visible: listofTodo.isNotEmpty,
+        replacement: Center(child: Text("Empty List")),
+        child: ListView.builder(
+            itemCount: listofTodo.length,
+            itemBuilder: (context, index) {
+              Todo todo = listofTodo[index];
+              return ListTile(
+                title: Text(todo.title),
+                subtitle: Text(todo.description),
+                leading: Text(todo.status.name),
+                trailing: Wrap(
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          _deleteTodo(index);
+                        },
+                        icon: Icon(Icons.delete)),
+                    IconButton(
+                        onPressed:() => _showStatusDialog(index), icon: Icon(Icons.edit)),
+                  ],
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => updateTodo(todo:todo,
+                        onUpdateTodo: (Todo updateTodo) {
+                          _updateTodo(index, updateTodo);
+                        },
+                      )));
+                },
+              );
+            }),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => const AddNewTodo()));
+        onPressed: () async {
+          Todo? todo = await Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const AddNewTodo()));
+          if (todo != null) {
+            _addTodo(todo);
+          }
         },
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void _showStatusDialog(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Change Status"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: const Text("Idle"),
+                onTap: () {
+                  _onTapUpdateStatus(index, TodoStatus.idle);
+                },
+              ),
+              const Divider(
+                height: 0,
+              ),
+              ListTile(
+                title: const Text("In Progress"),
+                onTap: () {
+                  _onTapUpdateStatus(index, TodoStatus.inProgress);
+                },
+              ),
+              const Divider(
+                height: 0,
+              ),
+              ListTile(
+                title: Text("Done"),
+                onTap: () {
+                  _onTapUpdateStatus(index, TodoStatus.done);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _onTapUpdateStatus(int index, TodoStatus status) {
+    _updateTodoStatus(index, status);
+    Navigator.pop(context);
   }
 }
--- lib/Screens/update_todo.dart
+++ lib/Screens/update_todo.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:ostad_rafat/Screens/todo.dart';
 
-class AddNewTodo extends StatefulWidget {
-  const AddNewTodo({super.key});
+class updateTodo extends StatefulWidget {
+  const updateTodo({super.key, required this.todo, required this.onUpdateTodo});
+
+  final Todo todo;
+  final Function (Todo) onUpdateTodo;
 
   @override
-  State<AddNewTodo> createState() => _AddNewTodoState();
+  State<updateTodo> createState() => _AddNewTodoState();
 }
 
-class _AddNewTodoState extends State<AddNewTodo> {
+class _AddNewTodoState extends State<updateTodo> {
   final TextEditingController _titleEditingController = TextEditingController();
   final TextEditingController _descriptionEditingController =
-      TextEditingController();
+  TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _titleEditingController.text = widget.todo.title;
+    _descriptionEditingController.text = widget.todo.description;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +35,7 @@ class _AddNewTodoState extends State<AddNewTodo> {
                   Navigator.pop(context);
                 },
                 icon: Icon(Icons.arrow_back)),
-            title: Text('Add New Todo'),
+            title: Text('Update To-Do'),
           ),
           body: Form(
             key: _formKey,
@@ -44,7 +54,9 @@ class _AddNewTodoState extends State<AddNewTodo> {
                       hintText: 'Enter a title for your todo',
                     ),
                     validator: (String? value) {
-                      if (value == null || value.trim().isEmpty) {
+                      if (value == null || value
+                          .trim()
+                          .isEmpty) {
                         return 'Title is required';
                       }
                       return null;
@@ -61,7 +73,9 @@ class _AddNewTodoState extends State<AddNewTodo> {
                         labelText: 'Description',
                         hintText: 'Enter description for your todo'),
                     validator: (String? value) {
-                      if (value == null || value.trim().isEmpty) {
+                      if (value == null || value
+                          .trim()
+                          .isEmpty) {
                         return 'Description is required';
                       }
                       return null;
@@ -75,13 +89,14 @@ class _AddNewTodoState extends State<AddNewTodo> {
                         if (_formKey.currentState!.validate()) {
                           Todo todo = Todo(
                             title: _titleEditingController.text.trim(),
-                            description:
-                                _descriptionEditingController.text.trim(),
-                          );
-                          Navigator.pop(context, todo);
+                            description: _descriptionEditingController.text
+                                .trim(),
+                          status: widget.todo.status);
+                          widget.onUpdateTodo(todo);
+                          Navigator.pop(context);
                         }
                       },
-                      child: Text("Add")),
+                      child: Text("Update")),
                 ],
               ),
             ),
