d6dc2656f068c004d3d28b536691a378cb9ba410
Rasheduzzaman Rochi <rasheduzzaman2003@gmail.com>
2024-12-21 04:05:55 +0600

    Added todo_manager.dart

---
+++ lib/models/todo_manager.dart
@@ -0,0 +1,21 @@
+import '../Screens/todo.dart';
+
+class TodoManager{
+  final List<Todo> _listofTodo = [];
+
+  void addTodo(Todo todo) {
+    _listofTodo.add(todo);
+  }
+
+  void deleteTodo(int index) {
+    _listofTodo.removeAt(index);
+  }
+
+  void updateTodo(int index, Todo todo) {
+    _listofTodo[index] = todo;
+  }
+
+  void updateTodoStatus(int index, TodoStatus status) {
+    _listofTodo[index].status = status;
+  }
+}
